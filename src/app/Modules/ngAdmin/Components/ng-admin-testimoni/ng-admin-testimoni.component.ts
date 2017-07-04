cb2932c9068acf113d4339710140f529cc95f5fa
Rusman Wahab <hwalyonglance>
2017-07-04 23:40:05 +0700

    experimental: readfile: input:file

--- src/app/Modules/ngAdmin/Components/ng-admin-testimoni/ng-admin-testimoni.component.ts
+++ src/app/Modules/ngAdmin/Components/ng-admin-testimoni/ng-admin-testimoni.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
 @Component({
 	selector: 'app-ng-admin-testimoni',
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
 	styleUrls: ['./ng-admin-testimoni.component.scss']
 })
 export class NgAdminTestimoniComponent {
-	constructor() {}
+	@ViewChild('field') itf: any;
+	@ViewChild('txtid') txtid: any;
+	constructor() {
+	}
+	up() {
+		const itf = this.itf;
+		console.log(itf);
+		const txtid = this.txtid;
+		setTimeout(function() {
+			console.log(itf);
+			console.log(itf.nativeElement.files);
+			const fr = new FileReader();
+			fr.onload = function (fileLoadedEvent) {
+				console.log(fileLoadedEvent.target);
+				txtid.nativeElement.innerHTML = fileLoadedEvent.target['result'];
+				alert(fileLoadedEvent.target['result'].length);
+			}
+			fr.readAsDataURL(itf.nativeElement.files[0]);
+		}, 5000);
+	}
 }
